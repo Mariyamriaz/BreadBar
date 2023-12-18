758725dac33a821ccef42b4580e093d2cc5832de
Maryam Riaz <104584580+Mariyamriaz@users.noreply.github.com>
2023-12-18 18:59:13 +0500

    user model

---
+++ Models/Usermodel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  // Add more fields as needed
+  resetToken: String,
+  resetTokenExpiry: Date,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
